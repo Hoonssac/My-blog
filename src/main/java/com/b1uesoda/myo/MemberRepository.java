65ffdfbcc74381ae433566e455028018817d6ea2
b1uesoda <b1uesoda@naver.com>
2024-05-04 23:29:16 +0900

    ã엔티티 구구성하기

--- src/main/java/com/b1uesoda/myo/MemberRepository.java
+++
@@ -1,8 +0,0 @@
-package com.b1uesoda.myo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MemberRepository extends JpaRepository<Member, Long> {
-}
