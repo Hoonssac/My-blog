7a8686512cc4a9a1d57cdeddb25bfbe04408b66e
b1uesoda <b1uesoda@naver.com>
2024-05-07 23:30:31 +0900

    컨트롤러 메서드 ì작성

---
+++ src/main/java/com/b1uesoda/myo/controller/BlogApiController.java
@@ -0,0 +1,26 @@
+package com.b1uesoda.myo.controller;
+
+import com.b1uesoda.myo.domain.Article;
+import com.b1uesoda.myo.dto.AddArticleRequest;
+import com.b1uesoda.myo.service.BlogService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController // HTTP Response Body에 객체 데이터를 JSON 형식으로 반환하는 컨트롤러
+public class BlogApiController {
+    private final BlogService blogService;
+
+    // HTTP 메서드가 POST일 때 전달받은 URL과 동일하면 메서드로 매핑
+    @PostMapping("/api/articles")
+    // @RequestBody로 요청 본문 값 매핑
+    public ResponseEntity<Article> addArticle(@RequestBody AddArticleRequest request) {
+        Article savedArticle = blogService.save(request);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(savedArticle);
+    }
+}
