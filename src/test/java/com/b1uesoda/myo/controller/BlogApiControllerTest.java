18ee2bdf2748c0145e86dc30565c4cb4b9034727
b1uesoda <b1uesoda@naver.com>
2024-05-17 16:26:32 +0900

    Article save test code

---
+++ src/test/java/com/b1uesoda/myo/controller/BlogApiControllerTest.java
@@ -0,0 +1,36 @@
+package com.b1uesoda.myo.controller;
+
+import com.b1uesoda.myo.repository.BlogRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class BlogApiControllerTest {
+
+    @Autowired
+    protected MockMvc mockMvc;
+
+    @Autowired
+    protected ObjectMapper objectMapper;
+
+    @Autowired
+    private WebApplicationContext context;
+
+    @Autowired
+    BlogRepository blogRepository;
+
+    @BeforeEach
+    public void mockMvcSetup() {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(context)
+            .build();
+        blogRepository.deleteAll();
+    }
+
+}
