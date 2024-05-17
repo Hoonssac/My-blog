d95ee93dd474f6d7d23df9c69f7e6c6211f4b6f3
b1uesoda <b1uesoda@naver.com>
2024-05-17 16:27:27 +0900

    add comment

--- src/test/java/com/b1uesoda/myo/controller/BlogApiControllerTest.java
+++ src/test/java/com/b1uesoda/myo/controller/BlogApiControllerTest.java
@@ -10,15 +10,15 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
-@SpringBootTest
-@AutoConfigureMockMvc
+@SpringBootTest // 테스트용 애플리케이션 컨텍스트
+@AutoConfigureMockMvc // MockMvc 생성 및 자동 구성
 class BlogApiControllerTest {
 
     @Autowired
     protected MockMvc mockMvc;
 
     @Autowired
-    protected ObjectMapper objectMapper;
+    protected ObjectMapper objectMapper; // 직렬화, 역직렬화를 위한 클래스
 
     @Autowired
     private WebApplicationContext context;
@@ -26,7 +26,7 @@ class BlogApiControllerTest {
     @Autowired
     BlogRepository blogRepository;
 
-    @BeforeEach
+    @BeforeEach // 테스트 실행 전 실행하는 메서드
     public void mockMvcSetup() {
         this.mockMvc = MockMvcBuilders.webAppContextSetup(context)
             .build();
