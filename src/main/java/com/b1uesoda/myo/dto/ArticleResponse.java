872b238855082167b5604b10026c5d2fb4de94c9
b1uesoda <b1uesoda@naver.com>
2024-05-21 23:43:18 +0900

    add Article Response Dto

---
+++ src/main/java/com/b1uesoda/myo/dto/ArticleResponse.java
@@ -0,0 +1,16 @@
+package com.b1uesoda.myo.dto;
+
+import com.b1uesoda.myo.domain.Article;
+import lombok.Getter;
+
+@Getter
+public class ArticleResponse {
+
+    private final String title;
+    private final String content;
+
+    public ArticleResponse(Article article) {
+        this.title = article.getTitle();
+        this.content = article.getContent();
+    }
+}
