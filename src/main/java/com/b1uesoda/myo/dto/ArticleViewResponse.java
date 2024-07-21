6477f6574188554ff50cb8f5829833dc6fb62eb4
b1uesoda <b1uesoda@naver.com>
2024-07-21 23:45:41 +0900

    return article of id

--- src/main/java/com/b1uesoda/myo/dto/ArticleViewResponse.java
+++ src/main/java/com/b1uesoda/myo/dto/ArticleViewResponse.java
@@ -1,2 +1,23 @@
-package com.b1uesoda.myo.dto;public class ArticleViewResponse {
+package com.b1uesoda.myo.dto;
+
+import com.b1uesoda.myo.domain.Article;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@NoArgsConstructor
+@Getter
+public class ArticleViewResponse {
+    private Long id;
+    private String title;
+    private String content;
+    private LocalDateTime createdAt;
+
+    public ArticleViewResponse(Article article) {
+        this.id = article.getId();
+        this.title = article.getTitle();
+        this.content = article.getContent();
+        this.createdAt =article.getCreatedAt();
+    }
 }
