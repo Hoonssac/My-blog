fd490a551dad028c6f0a223af77d172388d6fe3f
b1uesoda <b1uesoda@naver.com>
2024-07-29 23:24:42 +0900

    view specific article method

--- src/main/java/com/b1uesoda/myo/controller/BlogViewController.java
+++ src/main/java/com/b1uesoda/myo/controller/BlogViewController.java
@@ -1,11 +1,14 @@
 package com.b1uesoda.myo.controller;
 
+import com.b1uesoda.myo.domain.Article;
 import com.b1uesoda.myo.dto.ArticleListViewResponse;
+import com.b1uesoda.myo.dto.ArticleViewResponse;
 import com.b1uesoda.myo.service.BlogService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
@@ -24,4 +27,13 @@ public class BlogViewController {
 
         return "articleList";
     }
+
+    @GetMapping("/articles/{id}")
+    public String getArticle(@PathVariable Long id, Model model) {
+        Article article = blogService.findById(id);
+        model.addAttribute("article", new ArticleViewResponse(article));
+
+        return "article";
+    }
+
 }
