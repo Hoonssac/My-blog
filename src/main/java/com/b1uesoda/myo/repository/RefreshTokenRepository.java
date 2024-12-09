9841ec0d9446d9a1344154c884fa5b8cecdb767b
b1uesoda <b1uesoda@naver.com>
2024-12-09 10:09:54 +0900

    RefreshToken domain

---
+++ src/main/java/com/b1uesoda/myo/repository/RefreshTokenRepository.java
@@ -0,0 +1,11 @@
+package com.b1uesoda.myo.repository;
+
+import com.b1uesoda.myo.domain.RefreshToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
+    Optional<RefreshToken> findByUserId(Long userId);
+    Optional<RefreshToken> findByRefreshToken(String refreshToken);
+}
