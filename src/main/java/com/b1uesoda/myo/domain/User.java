6d318a06dff88861c23f5bc54f2fdff39b233791
b1uesoda <b1uesoda@naver.com>
2024-12-23 21:03:47 +0900

    OAuth2 Service

--- src/main/java/com/b1uesoda/myo/domain/User.java
+++ src/main/java/com/b1uesoda/myo/domain/User.java
@@ -29,10 +29,15 @@ public class User implements UserDetails { // UserDetails를 상속받아 인증
     @Column(name = "password")
     private String password;
 
+    // 사용자 이름
+    @Column(name = "nickname")
+    private String nickname;
+
     @Builder
-    public User(String email, String password, String auth) {
+    public User(String email, String password, String nickname, String auth) {
         this.email = email;
         this.password = password;
+        this.nickname = nickname;
     }
 
     @Override // 권한 반환
@@ -80,4 +85,9 @@ public class User implements UserDetails { // UserDetails를 상속받아 인증
         return true; // true -> 사용 가능
     }
 
+    // 사용자 이름 변경
+    public User update(String nickname) {
+        this.nickname = nickname;
+        return this;
+    }
 }
